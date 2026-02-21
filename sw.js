23173fe2d28d1f9ff99477a586bbc51331ba5692
saannndec5ty <stokakunsanzz250@gmail.com>
2026-02-22 04:02:38 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,10 @@
-const CACHE_NAME = 'sann404-music-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
-];
+const CACHE_NAME = 'sann404-lyrics-v3';
+const urlsToCache = [ '/', '/index.html', '/manifest.json' ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
